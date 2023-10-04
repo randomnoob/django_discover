0436c7575c73d45fbf718fe962659877c3f352b6
247 <you@example.com>
2023-10-04 16:53:47 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   content/admin.py
    	modified:   content/models.py

--- content/admin.py
+++ content/admin.py
@@ -5,13 +5,13 @@ from . import models
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'display_category', 'publish']
-    list_editable = ('publish',)
-    list_filter = ('publish', 'category',)
+    list_display = ['title', 'display_category', 'status']
+    list_editable = ('status',)
+    list_filter = ('status', 'category',)
     search_fields = ('title',)
     models.Post.display_category.short_description = _('Categories')
     prepopulated_fields = {'slug': ('title',)}
 
 
 admin.site.register(models.PostCategory)
-# admin.site.register(models.Post, PostAdmin)
+admin.site.register(models.Post, PostAdmin)
--- content/models.py
+++ content/models.py
@@ -1,45 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
 from ckeditor_uploader.fields import RichTextUploadingField
 
-
-class Post(models.Model):
-    title = models.CharField(
-        max_length=256,
-        verbose_name=_('Title :'),
-        unique=True,
-        null=False,
-        blank=False
-    )
-    slug = models.SlugField(
-        max_length=256,
-        verbose_name=_('Slug :'),
-        unique=True,
-        null=False,
-        blank=False
-    )
-    thumbnail = models.ImageField(
-        upload_to='content/Post/thumbnail/',
-        verbose_name=_('Thumbnail :')
-    )
-    publish = models.BooleanField(
-        verbose_name=_('Publish :'),
-        default=True,
-        help_text=_('Will this post be published?')
-    )
-
-    content = RichTextUploadingField()
-
-    class Meta:
-        verbose_name = _('Post')
-        verbose_name_plural = _('Posts')
-
-    def display_category(self):
-        return ', '.join([cat.title for cat in self.category.all()])
-
-    def __str__(self):
-        return self.title
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish")
+)
 
 
 class PostCategory(models.Model):
@@ -57,11 +25,53 @@ class PostCategory(models.Model):
         null=False,
         blank=False
     )
-    posts = models.ForeignKey(Post, on_delete=models.CASCADE)
+
     class Meta:
         verbose_name = _('Post Category')
         verbose_name_plural = _('Post Categories')
 
     def __str__(self):
         return self.title
+
+
+class Post(models.Model):
+    title = models.CharField(
+        max_length=256,
+        verbose_name=_('Title :'),
+        unique=True,
+        null=False,
+        blank=False
+    )
+    slug = models.SlugField(
+        max_length=256,
+        verbose_name=_('Slug :'),
+        unique=True,
+        null=False,
+        blank=False
+    )
+    category = models.ForeignKey(
+        PostCategory,
+        on_delete=models.CASCADE,
+        related_name='blog_posts'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete= models.CASCADE,
+        related_name='blog_posts'
+    )
+
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        verbose_name = _('Post')
+        verbose_name_plural = _('Posts')
+
+    def display_category(self):
+        return ', '.join([cat.title for cat in self.category.all()])
+
+    def __str__(self):
+        return self.title
+
 
