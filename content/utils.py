52f3cb73879014763db7b27f82baf8dabf059e03
247 <you@example.com>
2023-10-18 23:58:11 +0700

    fix utils.py

--- content/utils.py
+++ content/utils.py
@@ -49,15 +49,12 @@ def insert_internal_links(html, linklist):
         # then add the </p> tags back
         html_split = [x+"</p>" for x in re.split("</p>", html)]
         # Insert after first paragraph and before last one
-        code = '<p>Xem thêm: <a href="{}">{}</a><p>'.format(*linklist[0])
-        code2 = '<p>Xem thêm: <a href="{}">{}</a><p>'.format(*linklist[1])
-        html_split.insert(1, code)
-        html_split.insert(-3, code2)
-        # If document is long >6000 chars, insert one more in the middle
         try:
-            index = int(len(html_split)/2)-1
-            code3 = '<p>Xem thêm: <a href="{}">{}</a><p>'.format(*linklist[2])
-            html_split.insert(index, code3)
+            _build_internal_links(linklist[0], html_split, 1)
+            if len(html) <= 6000:
+                _build_internal_links(linklist[1], html_split, -3)
+            else:
+                _build_internal_links(linklist[2], html_split, int(len(html_split)/2)-1)
         except:
             traceback.print_exc()
             pass
@@ -67,36 +64,13 @@ def insert_internal_links(html, linklist):
     except:
         return html
 
+def _build_internal_links(linklist_element, html_split, index):
     """
-    Insert internal links to html
-    Preferably after the first paragraph, before the last one
-    and somewhere in between
-    linklist should be a NESTED LIST which follows this format : 
-    [
-        ['/abcxyz1', 'Title1'],
-        ['/abcxyz2', 'Title2'],
-    ]
+    Build the internal link code
+    insert it at index position
+    in html_split list
+    returns nothing
     """
-    try:
-        # Find all the </p> tags
-        ptags = [x for x in re.finditer("</p>", html)]
-        # Get the first, last and middle positions
-        # If the document is long, choose 3 positions, otherwise only 2
-        if len(html) >= 6000:
-            f_l_m = [ptags[0], ptags[int(len(ptags)/2)], ptags[-2]]
-        else:
-            f_l_m = [ptags[0], ptags[-2]]
-        f_l_m = list(set(f_l_m))
-        pos = [x.end(0) for x in f_l_m]
-        # cut the strings into parts
-        parts = [html[i:j] for i, j in zip([None] + pos, pos + [None])]
-        # For each position, add the href
-        for index in pos:
-            # Generate the html manually
-            code = f'<p>Xem thêm: <a href="{linklist[0][0]}">{linklist[0][1]}</a><p>'
-            # Drop the element we've just inserted
-            linklist = linklist[1:]
-            html = html[:index] + code + html[index:]
-    except:
-        pass
-    return html
+    code = '<p>Xem thêm: <a href="{}">{}</a><p>'.format(*linklist_element)
+    html_split.insert(index, code)
+
